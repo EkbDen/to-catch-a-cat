fea5cb22720f7f4fbed497ef9621c9108347a11e
Denis <ekb.den@mail.ru>
2024-10-06 19:21:10 +0500

    Add files via upload

--- main.c
+++ main.c
@@ -3,14 +3,17 @@
 #include <stdlib.h>
 #include <time.h>
 
+struct animal { int x, y; } dog, cat, prev_dog;
+
 int main()
 {
 	char map[10][21];
 	char key;
-	int dog_x = 10, dog_y = 5;
-	int dog_x_prev, dog_y_prev;
-	int cat_x = 3, cat_y = 3;
 	int score = 0;
+	
+	dog.x = 10, dog.y = 5;
+	cat.x = 3, cat.y = 3;	
+	
 	
 	// Рандомность чисел
 	srand(time(NULL));
@@ -23,8 +26,8 @@ int main()
 		sprintf(map[9], "####################");
 		
 		// Рисуем собаку и кошку
-		map[dog_y][dog_x] = '@';
-		map[cat_y][cat_x] = 'c';
+		map[dog.y][dog.x] = '@';
+		map[cat.y][cat.x] = 'c';
 		
 		// Очищаем экран
 		system("cls");	
@@ -40,21 +43,21 @@ int main()
 		printf("Score: %d\n", score);
 				
 		// Предыдущее местоположение равно нынешнему положению
-		dog_x_prev = dog_x;
-		dog_y_prev = dog_y;		
+		prev_dog.x = dog.x;
+		prev_dog.y = dog.y;		
 		
 		// Проверяем нажатие клавиши
 		key = getch();
-		if (key == 'w') { dog_y--; }
-		if (key == 's') { dog_y++; }
-		if (key == 'a') { dog_x--; }
-		if (key == 'd') { dog_x++; }
+		if (key == 'w') { dog.y--; }
+		if (key == 's') { dog.y++; }
+		if (key == 'a') { dog.x--; }
+		if (key == 'd') { dog.x++; }
 		
 		// Проверяем коллизию со стенами
-		if ( map[dog_y][dog_x] == '#' ) { dog_x = dog_x_prev; dog_y = dog_y_prev; }
+		if ( map[dog.y][dog.x] == '#' ) { dog.x = prev_dog.x; dog.y = prev_dog.y; }
 		
 		// Проверяем коллизию с котом
-		if ( (dog_x == cat_x) && (dog_y == cat_y) ) { cat_x = rand() % 18 + 1; cat_y = rand() % 8 + 1; score++; }
+		if ( (dog.x == cat.x) && (dog.y == cat.y) ) { cat.x = rand() % 18 + 1; cat.y = rand() % 8 + 1; score++; }
 	}	
 	
 	return 0;
